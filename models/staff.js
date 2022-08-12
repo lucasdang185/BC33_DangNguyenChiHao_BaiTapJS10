4eda47197b71ae9b61e87e9cd3582192abb02e7c
lucasdang <dangnguyenchihao1@gmail.com>
2022-08-12 20:07:52 +0700

    push edit del

---
+++ models/staff.js
@@ -0,0 +1,11 @@
+/** Đối tượng nhân viên */
+function NhanVien(id, name, email, pass, day, salary, level, workTime){
+    this.id =id;
+    this.name =name;
+    this.email =email;
+    this.pass =pass;
+    this.day =day;
+    this.salary =salary;
+    this.level =level;
+    this.workTime =workTime;
+}
